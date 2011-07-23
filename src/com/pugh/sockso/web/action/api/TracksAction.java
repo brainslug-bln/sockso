507a169a7a5048b5d459d1dc0288ad166db1de89
rod <rod@pu-gh.com>
2011-07-23 10:04:20 +0100

    added comments

--- src/com/pugh/sockso/web/action/api/TracksAction.java
+++ src/com/pugh/sockso/web/action/api/TracksAction.java
@@ -11,6 +11,13 @@ import java.util.Vector;
 
 public class TracksAction extends BaseApiAction {
 
+    /**
+     *  Shows tracks as JSON
+     * 
+     *  @throws Exception 
+     * 
+     */
+    
     public void handleRequest() throws Exception {
 
         Vector<Track> tracks = Track.findAll(
@@ -23,6 +30,15 @@ public class TracksAction extends BaseApiAction {
         
     }
     
+    /**
+     *  Renders tracks to response
+     * 
+     *  @param tracks
+     * 
+     *  @throws IOException 
+     * 
+     */
+    
     protected void showTracks( final Vector<Track> tracks ) throws IOException {
         
         TTracks tpl = new TTracks();
@@ -32,6 +48,15 @@ public class TracksAction extends BaseApiAction {
         
     }
 
+    /**
+     *  Indicates if this action can handle the request
+     * 
+     *  @param req
+     * 
+     *  @return 
+     * 
+     */
+    
     public boolean canHandle( final Request req ) {
         
         return req.getParamCount() == 2
